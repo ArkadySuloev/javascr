160dcced33cabe028aebe2a06d92aeae6b297d67
Arkady <skaterCH87@yandex.ru>
2018-03-04 16:26:46 +0300

    Initial commit

---
+++ while.js
@@ -0,0 +1,11 @@
+var log = 2833839448;
+var pass = loh3320344;
+
+var LogInput = +prompt("Введите логин")
+var passInput = +prompt("Введите пароль")
+
+alert(LogInput);
+alert(passInput);
+while (LogInput = log , passInput = pass) {
+    alert("Выполнен вход")
+}
